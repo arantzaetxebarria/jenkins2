94e2c258cea739d762adc4289211fb83a503a488
Your Name <you@example.com>
2021-05-18 14:00:40 +0200

    test

--- src/main/java/curso/net/Calculadora/Calculadora.java
+++ src/main/java/curso/net/Calculadora/Calculadora.java
@@ -2,7 +2,7 @@ package curso.net.calculadora;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Calculator {
+public class Calculadora {
 int sum(int a, int b) {
 return a + b;
 }
